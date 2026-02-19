2d85d71a4f8f622fee3cb304e05516bf26892bfb
Svitlana Melnyk <drrttlotr@ukr.net>
2026-02-19 16:46:53 +0200

    Updates

--- task2.py
+++ task2.py
@@ -1,19 +1,15 @@
 import re
 from typing import Callable
 
-def generator_numbers(text: str) -> list:
-    pattern = '([0-9]+(?:\\.[0-9]+)?)(?:\\s)'
+def generator_numbers(text: str):
+    pattern = '(?:\\s)([0-9]+(?:\\.[0-9]+)?)(?:\\s)'
     list = re.findall(pattern, text)
-    return list
+    for number in list:
+        yield number
 
 def sum_profit(text: str, func: Callable):
-    incomes = func(text)
-    sum = 0
-
-    for income in incomes:
-        sum = sum + float(income)
-
-    return sum
+    income = func(text)
+    return sum(list(map(float, income)))
 
 text = 'Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів.'
 total_income = sum_profit(text, generator_numbers)
